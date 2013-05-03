2e031c1fb4ee2848fcc99a4833e68afda9d3e875
Nigel Hughes <nhughes@Retina-MacBook-Pro.local>
2013-05-03 20:58:16 +0100

    XCode Initial Commit

---
+++ RWEBind/RWEBind/RWEBind.h
@@ -0,0 +1,13 @@
+//
+//  RWEBind.h
+//  RWEBind
+//
+//  Created by Nigel Hughes on 03/05/2013.
+//  Copyright (c) 2013 RED When Excited Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RWEBind : NSObject
+
+@end
---
+++ RWEBind/RWEBind/RWEBind.m
@@ -0,0 +1,13 @@
+//
+//  RWEBind.m
+//  RWEBind
+//
+//  Created by Nigel Hughes on 03/05/2013.
+//  Copyright (c) 2013 RED When Excited Ltd. All rights reserved.
+//
+
+#import "RWEBind.h"
+
+@implementation RWEBind
+
+@end
